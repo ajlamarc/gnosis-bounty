89680b60f4e0bffc60588afa3500c6652a7a95a6
AJ LaMarc <ajlamarc@gmail.com>
2023-05-16 07:18:32 -0400

    hardcoded zod localhost 8080 for now

--- frontend/src/hooks/useUrbit.ts
+++ frontend/src/hooks/useUrbit.ts
@@ -1,11 +1,13 @@
 import { Urbit } from '@urbit/http-api'
-const api = new Urbit('', '', 'gnosis')
-
+const api = new Urbit('http://localhost:8080', 'lidlut-tabwed-pillex-ridrup', 'safe')
+// const api = new Urbit('', '', 'safe')
 if (typeof window !== 'undefined') {
   // @ts-ignore
   console.log('inside useUrbit: ', window.ship)
   // @ts-ignore
-  api.ship = window.ship
+  //api.ship = window.ship
+  api.ship = 'zod'
+  api.verbose = true
 }
 
 // @ts-ignore
